78ec952a7ad0c15f557306394d16a1116b0b5301
Nand Patel <nand.pats@gmail.com>
2024-09-14 19:06:56 -0700

    add first page

---
+++ app/costumes/ideas/page.js
@@ -0,0 +1,26 @@
+"use client";
+import { useEffect, useState } from "react";
+import { fetchIdea } from "./actions";
+
+export default function Ideas() {
+  let [idea, setIdea] = useState();
+
+  async function handleNewIdea() {
+    setIdea(fetchIdea());
+  }
+
+  return (
+    idea !== "" && (
+      <div className="flex h-full">
+        <div className="flex-1 m-auto">
+          <div className="text-center m-5 p-5 outline outline-black outline-4">
+            {idea ? idea : "click button to see a random costume idea"}
+          </div>
+          <div className="text-center">
+            <button onClick={handleNewIdea}>new idea</button>
+          </div>
+        </div>
+      </div>
+    )
+  );
+}
