36d3dfa8b807eaf06b8e86e1f2df327e66733a5a
Nand Patel <nand.pats@gmail.com>
2024-09-26 17:53:47 -0700

    add scroll to top component to fix scroll problem

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
-import NavBar from "@/components/navbar";
+import NavBar from "@/components/NavBar";
 import localFont from "next/font/local";
 import "./globals.css";
+import ScrollToTop from "@/components/ScrollToTop";
 
 const fraunces = localFont({ src: "./fonts/Fraunces--latin_basic.woff2" });
 
@@ -11,6 +12,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <ScrollToTop />
       <body
         className={`bg-background ${fraunces.className} font-thin text-2xl`}
         style={{ fontVariationSettings: `"WONK" 1` }}
